032c347694ef2ca37ff9f02b663650d4f7630a0a
Kevin Coughlin <kevintcoughlin@gmail.com>
2017-11-26 11:36:39 -0800

    Add endpoint enum

--- feature/src/main/java/com/kevintcoughlin/smodr/feature/MainActivity.kt
+++ feature/src/main/java/com/kevintcoughlin/smodr/feature/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.widget.LinearLayout
+import com.kevintcoughlin.smodr.feature.network.FeedEndpoints
 import com.kevintcoughlin.smodr.feature.network.RssService
 import com.kevintcoughlin.smodr.feature.view.FeedAdapter
 import retrofit2.Retrofit
@@ -15,10 +16,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val retrofit = Retrofit.Builder().build()
-//        val rssService = retrofit.create(RssService::class.java)
-//        val resp = rssService.fetchFeed("http://feeds.feedburner.com/SModcasts")
-//
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://feeds.feedburner.com/")
+                .build()
+        val rssService = retrofit.create(RssService::class.java)
+        val feedUrl = FeedEndpoints.SMODCAST.url;
+        val resp = rssService.fetchFeed(feedUrl)
+
 //        val list = findViewById<RecyclerView>(R.id.list)
 //        val linearLayoutManager = LinearLayoutManager(this)
 //        val adapter = FeedAdapter()
---
+++ feature/src/main/java/com/kevintcoughlin/smodr/feature/network/FeedEndpoints.kt
@@ -0,0 +1,6 @@
+package com.kevintcoughlin.smodr.feature.network
+
+
+enum class FeedEndpoints(val url: String) {
+    SMODCAST("https://feeds.feedburner.com/SModcasts")
+}
